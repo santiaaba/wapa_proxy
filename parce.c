da80e3010f55e9fb824ac87b3a5a2f699ea6a84d
Santiago <santiaaba@hotmail.com>
2018-09-24 15:54:44 -0400

    avanzamos

--- parce.c
+++ parce.c
@@ -1,6 +1,6 @@
 #include "parce.h"
 
-void parce_data(char *buffer, int *i, char *value){
+void parce_data(char *buffer, char sep, int *i, char *value){
 	/* Dado un comando recibido desde el servidor con formato
 	   de campos separados por |, retorna el valor a partir
 	   de la posición dada hasta el proximo | o fin del string.
@@ -11,26 +11,10 @@ void parce_data(char *buffer, int *i, char *value){
 	int largo;
 
 	largo = strlen(buffer);
-	while(*i < largo && buffer[*i] != '|' && buffer[*i] != '\0'){
+	while(*i < largo && buffer[*i] != sep && buffer[*i] != '\0'){
 		value[j] = buffer[*i];
 		j++; (*i)++;
 	}
 	value[j]='\0';
-	/* Adelantamos la i una posicion ya que se
-	   encuentra parada en el caracter "|" posiblemente */
 	(*i)++;
-}
-char parce_get_action(char *buffer){
-	/* Dado el buffer retorna la accion a realizar. El mismo
-	   es el char en la posicion 2 */
-	return buffer[4];
-}
-void parce_get_taskid(char *buffer, char *value){
-	/* Dado un buffer, retorna el taskid como
-	   una secuencia de 2 bytes (dos char). Se
-	   encuentran en la posición 0 a la 1. */
-	value[0] = buffer[0];
-	value[1] = buffer[1];
-	value[2] = buffer[2];
-	value[3] = buffer[3];
 }
--- proxy_tool.c
+++ proxy_tool.c
@@ -4,28 +4,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "parce.h"
+#include "send_receive.h"
 #include "config.h"
 
-#define PORT 3550 /* El puerto que será abierto */
-#define BACKLOG 1 /* El número de conexiones permitidas */
 #define SITESD "/etc/nginx/sites.d/"
-#define BUFFER_SIZE 1024
 
 /********************************
 *      FUNCIONES	       *
 ********************************/
 
-void clear_buffer(char *buffer_rx){
-	int i;
-	for (i=0;i<BUFFER_SIZE;i++){
-		buffer_rx[i] = '\0';
-	}
-}
-
-char nginx_systemctl(int action){	//0 stop, 1 start, 2 reload
+void nginx_systemctl(int action, char **send_message, uint32_t *send_message_size){	//0 stop, 1 start, 2 reload
 	FILE *fp;
 	char status[2];
 
+	*send_message_size=2;
+	*send_message=(char *)realloc(*send_message,*send_message_size);
 	switch(action){
 		case 0:
 			fp = popen("systemctl stop nginx 2>/dev/null; echo $?", "r");
@@ -37,122 +30,112 @@ char nginx_systemctl(int action){	//0 stop, 1 start, 2 reload
 			fp = popen("systemctl reload nginx 2>/dev/null; echo $?", "r");
 			break;
 		default:
-			pclose(fp);
-			return 0;
+			strcpy(*send_message,"0");
 	}
 	fgets(status, sizeof(status)-1, fp);
 	pclose(fp);
-	return atoi(status);
+	strcpy(*send_message,"1");
 }
 
-int add_site(char *buffer_rx, int fd_client, T_config *c){
+int add_site(char *rcv_message, char **send_message, uint32_t *send_message_size, T_config *cfg){
 	/* Agrega un sitio a la configuracion del NGINX */
 	char site_name[100];
 	char aux[500];
-	int pos=2;
 	char alias[200];
 	FILE *fd;
 	char site_path[100];
-	char buffer_tx[BUFFER_SIZE];
+	char *aux_list = NULL;
+	int aux_list_size;
+	int pos = 1;
+	int posaux;
 
-
-	
-	parce_data(buffer_rx,&pos,site_name);
-
+	parce_data(rcv_message,'|',&pos,site_name);
 	sprintf(site_path,"%s%s.conf",SITESD,site_name);
 	printf("Abriendo archivo: %s\n",site_path);
-	fd = fopen(site_path,"w");
-	if(!fd){
-		printf("Problemas para escribir el archivo %s\n",site_path);
-		send(fd_client,"0", BUFFER_SIZE,0);
-		return 0;
-	}
-
-	parce_data(buffer_rx,&pos,aux); //site_id
-	fprintf(fd,"#ID %s\n",aux);
-	parce_data(buffer_rx,&pos,aux); //site_ver
-	fprintf(fd,"#VER %s\n",aux);
-
-	printf("PASO1\n");
-	/* Indicamos que estamos listos para recibir los workers */
-	/* Ojo que con esto eliminamos el buffer_rx anterior */
-	fprintf(fd,"upstream %s {\n",site_name);
-	do{
-		send(fd_client,"1", BUFFER_SIZE,0);
-		recv(fd_client,buffer_rx,BUFFER_SIZE,0);
-		pos=2;
-		while(pos < strlen(buffer_rx)){
-			parce_data(buffer_rx,&pos,aux);
+	if(fd = fopen(site_path,"w")){
+		parce_data(rcv_message,'|',&pos,aux); //site_id
+		fprintf(fd,"#ID %s\n",aux);
+		parce_data(rcv_message,'|',&pos,aux); //site_ver
+		fprintf(fd,"#VER %s\n",aux);
+	
+		printf("PASO1\n");
+		/* Indicamos que estamos listos para recibir los workers */
+		/* Ojo que con esto eliminamos el rcv_message anterior */
+		aux_list = (char *)malloc(strlen(rcv_message) - pos + 1);
+		parce_data(rcv_message,'|',&pos,aux_list);
+		aux_list_size = strlen(aux_list);
+		posaux=0;
+		fprintf(fd,"upstream %s {\n",site_name);
+		while(posaux < aux_list_size){
+			parce_data(aux_list,',',&posaux,aux);
 			fprintf(fd,"\tserver %s;\n",aux);
 		}
-		printf("buffer_rx -%s\n-",buffer_rx);
-	} while(buffer_rx[0] != '0');
-	printf("PASO2\n");
-
-	fprintf(fd,"}\n");
-	fprintf(fd,"server {\n");
-	fprintf(fd,"\tlisten 80;\n");
-	fprintf(fd,"\tserver_name %s.%s;\n",site_name,config_default_domain(c));
-
-	/* Aca van los server name/alias */
-	printf("PASO3\n");
-	do{
-		send(fd_client,"1", BUFFER_SIZE,0);
-		recv(fd_client,buffer_rx,BUFFER_SIZE,0);
-		pos=2;
-		while(pos < strlen(buffer_rx)){
-			parce_data(buffer_rx,&pos,aux);
+		fprintf(fd,"}\n");
+		fprintf(fd,"server {\n");
+		fprintf(fd,"\tlisten 80;\n");
+		fprintf(fd,"\tserver_name %s.%s;\n",site_name,config_default_domain(cfg));
+	
+		/* Aca van los server name/alias */
+		aux_list = (char *)malloc(strlen(rcv_message) - pos + 1);
+		parce_data(rcv_message,'|',&pos,aux_list);
+		aux_list_size = strlen(aux_list);
+		posaux=0;
+		while(posaux<aux_list_size){
+			parce_data(aux_list,',',&posaux,aux);
 			fprintf(fd,"\tserver_name %s;\n",aux);
 		}
-		printf("buffer_rx -%s\n-",buffer_rx);
-	} while(buffer_rx[0] != '0');
-	send(fd_client,"1", BUFFER_SIZE,0);
-	printf("PASO4\n");
-
-	fprintf(fd,"\tlocation / {\n");
-	fprintf(fd,"\t\tproxy_pass http://%s;\n",site_name);
-	fprintf(fd,"\t\tproxy_set_header X-Real-IP $remote_addr;\n");
-	fprintf(fd,"\t\tproxy_set_header Host $http_host;\n");
-	fprintf(fd,"\t}\n");
-	fprintf(fd,"}\n");
 	
-	printf("PASO5\n");
-	fclose(fd);
-	return 1;
+		fprintf(fd,"\tlocation / {\n");
+		fprintf(fd,"\t\tproxy_pass http://%s;\n",site_name);
+		fprintf(fd,"\t\tproxy_set_header X-Real-IP $remote_addr;\n");
+		fprintf(fd,"\t\tproxy_set_header Host $http_host;\n");
+		fprintf(fd,"\t}\n");
+		fprintf(fd,"}\n");
+	
+		fclose(fd);
+		sprintf(*send_message,"1");
+	} else {
+		sprintf(*send_message,"0");
+	}
 }
 
-void delete_site(char *buffer_rx, int fd_client){
+void delete_site(char *rcv_message, char **send_message, uint32_t *send_message_size){
 	/* Elimina el archivo de configuracion de un sitio */
 	FILE *fp;
 	char command[1000];
 	char aux[100];
-	int pos;
 
-	pos=2;
-	parce_data(buffer_rx,&pos,aux);
+	int pos=1;
+	parce_data(rcv_message,'|',&pos,aux);
 	sprintf(command,"rm %s%s.conf -f",SITESD,aux);
 	printf(command);
 	fp = popen(command, "r");
 	pclose(fp);
-	send(fd_client,"1", BUFFER_SIZE,0);
+	*send_message_size = 2;
+        *send_message = (char *)realloc(*send_message,*send_message_size);
+	sprintf(*send_message,"1");
 }
 
-void delete_all(char *buffer_rx, int fd_client){
+void delete_all(char *rcv_message, char **send_message, uint32_t *send_message_size){
 	/* Elimina la configuracion de sitios toda */
 	FILE *fp;
-        char command[1000];
+	char command[100];
 
 	sprintf(command,"rm %s* -f",SITESD);
 	printf(command);
 	fp = popen(command, "r");
 	pclose(fp);
-	send(fd_client,"1", BUFFER_SIZE,0);
+	*send_message_size = 2;
+        *send_message = (char *)realloc(*send_message,*send_message_size);
+	sprintf(*send_message,"1");
 }
 
-int check(char *detalle){
+void check(char **send_message, uint32_t *send_message_size){
 	FILE *fp;
-        char buffer[100];
-        int status = 1;
+	char buffer[100];
+	char detalle[300];
+	char aux[200];
+	char status = '1';
 
 	strcpy(detalle,"todo OK"); // De entrada esta todo bien
 	fp = popen("systemctl status nginx > /dev/null; echo $?", "r");
@@ -165,9 +148,43 @@ int check(char *detalle){
 
 	if(buffer[0] != '0'){
 		strcpy(detalle,"nginx caido");
-		status = 0;
+		status = '0';
 	}
-	return status;
+	*send_message_size = strlen(detalle) + 3; //el 3 incluye el estado , el "|" y el \0
+	*send_message=(char *)realloc(*send_message,*send_message_size);
+	sprintf(*send_message,"%c%s|",status,detalle);
+
+	/* Para la CPU */
+	fp = popen("uptime | awk '{print $10\"|\"$11\"|\"$12\"|\"}' | sed 's\\,\\\\g'", "r");
+	fgets(buffer, sizeof(buffer)-1, fp);
+	strcpy(aux,buffer);
+	aux[strlen(aux) - 1] = '\0';
+	pclose(fp);
+
+	/* Para la CPU */
+	fp = popen("vmstat | tail -1 | awk '{print $14\"|\"$13\"|\"$15\"|\"$16\"|\"}'", "r");
+	fgets(buffer, sizeof(buffer)-1, fp);
+	strcat(aux,buffer);
+	aux[strlen(aux) - 1] = '\0';
+	pclose(fp);
+
+	/* Para la memoria */
+	fp = popen("free | tail -2 | head -1 | awk '{print $2\"|\"$3\"|\"}'", "r");
+	fgets(buffer, sizeof(buffer)-1, fp);
+	strcat(aux,buffer);
+	aux[strlen(aux) - 1] = '\0';
+	pclose(fp);
+
+	/* Para la swap */
+	fp = popen("free | tail -1 | awk '{print $2\"|\"$3}'", "r");
+	fgets(buffer, sizeof(buffer)-1, fp);
+	strcat(aux,buffer);
+	aux[strlen(aux) - 1] = '\0';
+	pclose(fp);
+
+	*send_message_size += strlen(aux);
+	*send_message=(char *)realloc(*send_message,*send_message_size);
+	strcat(*send_message,aux);
 }
 
 void statistics(char *aux){
@@ -217,11 +234,14 @@ int repare(){
 int main(int argc , char *argv[]){
 
 	int fd_server, fd_client; /* los ficheros descriptores */
-	char buffer_rx[BUFFER_SIZE];
-	char buffer_tx[BUFFER_SIZE];
 	struct sockaddr_in server;
 	struct sockaddr_in client;
-	T_config config;
+	char *rcv_message=NULL;
+	int rcv_message_size;
+	char *send_message=NULL;
+	int send_message_size;
+
+	T_config cfg;
 	int sin_size;
 	int pos;
 	char aux[200];
@@ -229,7 +249,7 @@ int main(int argc , char *argv[]){
 	int result;
 	int cant_bytes;
 
-	config_load("proxy_tool.conf",&config);
+	config_load("proxy_tool.conf",&cfg);
 	
 	if ((fd_server=socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
 		printf("error en socket()\n");
@@ -258,72 +278,45 @@ int main(int argc , char *argv[]){
 		}
 
 		// Aguardamos continuamente que el cliente envie un comando
-		while(recv(fd_client,buffer_rx,BUFFER_SIZE,0)>0){
+		while(recv_all_message(fd_client,&rcv_message,&rcv_message_size)){
 			printf("Esperando conneccion desde el cliente()\n"); //Debemos mantener viva la conexion
-			printf("Recibimos -%s-\n",buffer_rx);
-			action = buffer_rx[0];
+			printf("Recibimos -%s-\n",rcv_message);
+			action = rcv_message[0];
 			switch(action){
 				case 'A':
 					printf("Agregamos sitio\n");
-					add_site(buffer_rx,fd_client,&config);
+					add_site(rcv_message,&send_message,&send_message_size,&cfg);
 					break;
 				case 'd':
-					delete_site(buffer_rx,fd_client);
+					delete_site(rcv_message,&send_message,&send_message_size);
 					break;
 				case 'D':
-					delete_all(buffer_rx,fd_client);
+					delete_all(rcv_message,&send_message,&send_message_size);
 					break;
 				case 'C':
 					printf("Chequeamos el worker\n");
-					if(check(aux) == 1){
-						buffer_tx[0] = '1';
-					} else {
-						buffer_tx[0] = '0';
-					}
-					buffer_tx[1] = '|'; buffer_tx[2] = '\0';
-					strcat(buffer_tx,aux);
-					statistics(aux);
-					strcat(buffer_tx,aux);
-
-					printf("Esperando que el cliente reciba los datos\n");
-					cant_bytes = send(fd_client,buffer_tx, BUFFER_SIZE,0);
-					printf("Enviamos(%i) -%s-\n",cant_bytes,buffer_tx);
+					check(&send_message,&send_message_size);
 					break;
 				case 'S':
 					/* Start Nginx */
-					if(nginx_systemctl(0) == 0){
-						buffer_tx[0] = 1;
-					} else {
-						buffer_tx[0] = 0;
-						}
-					buffer_tx[1] = '|'; buffer_tx[2] = '\0';
-					send(fd_client,buffer_tx, BUFFER_SIZE,0);
+					nginx_systemctl(0,&send_message,&send_message_size);
 					break;
 				case 'K':
 					/* Stop Nginx */
-					if(nginx_systemctl(1) == 0){
-						buffer_tx[0] = 1;
-					} else {
-						buffer_tx[0] = 0;
-						}
-					buffer_tx[1] = '|'; buffer_tx[2] = '\0';
-					send(fd_client,buffer_tx, BUFFER_SIZE,0);
+					nginx_systemctl(1,&send_message,&send_message_size);
 					break;
 				case 'R':
 					/* Reload Nginx */
-					if(nginx_systemctl(2) == 0){
-						buffer_tx[0] = 1;
-					} else {
-						buffer_tx[0] = 0;
-						}
-					buffer_tx[1] = '|'; buffer_tx[2] = '\0';
-					send(fd_client,buffer_tx, BUFFER_SIZE,0);
+					nginx_systemctl(2,&send_message,&send_message_size);
 					break;
 				default :
 					printf("Error protocolo\n");
-					send(fd_client,"0\0",BUFFER_SIZE,0);
+					send_message_size=2;
+					send_message = (char *)realloc(send_message,send_message_size);
+					sprintf(send_message,"0");
 			}
+			send_all_message(fd_client,send_message,send_message_size);
 		}
+		close(fd_client);
 	}
-	close(fd_client);
 }
